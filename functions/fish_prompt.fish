4a536806012f2e73f46e3d5fd97772cb238fd83b
Gabriella Bere <contact@gabbie.blue>
2025-06-04 15:08:46 +0100

    did the test wrong, should be okay now

--- functions/fish_prompt.fish
+++ functions/fish_prompt.fish
@@ -234,7 +234,9 @@ function prompt_virtual_env -d "Display Python or Nix virtual environment"
     # We check for this case after checking for "impure" because impure brings too many packages 
     # into PATH.
     set envs_unfiltered $nix_packages # passes nix packages to the filter function
-    test "$envs_filtered"; and set envs $envs "nix[$envs_filtered]" # appends the result to $envs, but only if theres packages present
+    if test "$envs_filtered"
+      set envs $envs "nix[$envs_filtered]" # appends the result to $envs, but only if theres packages present
+    end
   else if test "$IN_NIX_SHELL"
     # Support for `nix-shell --pure`. Would print "nix[pure]".
     # We check for this case after checking for individual packages because it otherwise might 
